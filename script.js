7f21d2eec7c020b10fc6fa38656d95b1f47aa5f0
oat phattaraphon <phattaraphon.c@ku.th>
2020-08-22 02:12:05 +0700

    initial commit v1

---
+++ script.js
@@ -0,0 +1,81 @@
+let table = document.getElementById('countries_stat');
+
+// Fetching the World Data
+
+fetch('https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php', {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+    'x-rapidapi-key': 'daf35fb907msh5943652ba5bddf2p1aa387jsnd6bac0794a2b',
+  },
+})
+  .then((response) =>
+    response.json().then((data) => {
+      //   console.log('====================================');
+      //   console.log(data);
+      //   console.log('====================================');
+      let new_cases = document.getElementById('new_case');
+      let new_death = document.getElementById('new_death');
+      let total_death = document.getElementById('total_death');
+      let total_recovered = document.getElementById('total_recovered');
+      let total_cases = document.getElementById('total_cases');
+
+      total_cases.innerHTML = data.total_cases;
+      new_cases.innerHTML = data.new_cases;
+      new_death.innerHTML = data.new_deaths;
+      total_death.innerHTML = data.total_deaths;
+      total_recovered.innerHTML = data.total_recovered;
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Fetching the case by country data
+fetch(
+  'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php',
+  {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+      'x-rapidapi-key': 'daf35fb907msh5943652ba5bddf2p1aa387jsnd6bac0794a2b',
+    },
+  }
+)
+  .then((response) =>
+    response.json().then((data) => {
+      //   console.log('====================================');
+      console.log(data);
+      //   console.log('====================================');
+
+      let countries_stat = data.countries_stat;
+      //   console.log(countries_stat);
+      for (let i = 0; i < countries_stat.length; i++) {
+        // console.log(countries_stat[i]);
+        let row = table.insertRow(i + 1);
+        let country_name = row.insertCell(0);
+        let cases = row.insertCell(1);
+        let deaths = row.insertCell(2);
+        let serious_critical = row.insertCell(3);
+        let recovered_per_country = row.insertCell(4);
+        let total_tested = row.insertCell(5);
+
+        country_name.innerHTML = countries_stat[i].country_name;
+        cases.innerHTML = countries_stat[i].cases;
+        deaths.innerHTML = countries_stat[i].deaths;
+        serious_critical.innerHTML = countries_stat[i].serious_critical;
+        recovered_per_country.innerHTML = countries_stat[i].total_recovered;
+        total_tested.innerHTML = countries_stat[i].total_tests;
+      }
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+
+$(document).ready(function () {
+  $('#countries_stat').DataTable({
+    paging: false, // false to disable pagination (or any other option)
+  });
+  $('.dataTables_length').addClass('bs-select');
+});
